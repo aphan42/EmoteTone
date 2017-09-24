aeea904bb205cd84257b14c0af8ac60d33a47cf2
Achintya Kattemalavadi <chinny@chinnyarch.localdomain>
2017-09-24 03:36:25 -0400

    Now with Firebase

--- app/src/main/java/com/example/chinny/mhacksxmusicplayer/MainActivity.java
+++ app/src/main/java/com/example/chinny/mhacksxmusicplayer/MainActivity.java
@@ -30,8 +30,15 @@ import android.util.SparseIntArray;
 import android.view.Surface;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -67,8 +74,12 @@ public class MainActivity extends AppCompatActivity {
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
 
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+
     private TextView emoteStatus;
     private FloatingActionButton captureFab;
+    private Button happyButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,6 +94,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 takePicture();
+            }
+        });
+        happyButton = (Button) findViewById(R.id.happyButton);
+        happyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlayHappyMusic();
             }
         });
     }
@@ -334,5 +352,24 @@ public class MainActivity extends AppCompatActivity {
         //closeCamera();
         stopBackgroundThread();
         super.onPause();
+    }
+
+    void PlayHappyMusic(){
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
